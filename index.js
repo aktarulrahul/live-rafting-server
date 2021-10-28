fe8fe2f4422de414b968b6a1d197f37d59ca7c9e
aktarulrahul <contact.aktarul@gmail.com>
2021-10-29 02:28:41 +0600

    implement booking and package get post delete api

--- index.js
+++ index.js
@@ -29,8 +29,84 @@ async function mongodbCURD() {
     database name and collection init
     ------------------------------------- */
     const database = client.db('liveRafting');
-    const bookingCollection = database.collection('booking');
-    const raftingCollection = database.collection('raftings');
+    const bookingCollection = database.collection('bookings');
+    const raftingPackageCollection = database.collection('raftingPackages');
+    /* ------------------------------------- 
+    GET All Rafting Package API
+    ------------------------------------- */
+    app.get('/rafting-packages', async (req, res) => {
+      const cursor = raftingPackageCollection.find({});
+      const raftingPackages = await cursor.toArray();
+      res.send(raftingPackages);
+    });
+    /* ------------------------------------- 
+    GET Single Rafting Package API
+    ------------------------------------- */
+    app.get('/rafting-packages/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const raftingPackage = await raftingPackageCollection.findOne(query);
+      res.json(raftingPackage);
+    });
+    /* ------------------------------------- 
+    POST Single Rafting Package API
+    ------------------------------------- */
+    app.post('/rafting-packages', async (req, res) => {
+      // Step 1. data
+      const raftingPackage = req.body;
+      // Step 2. insertOne
+      const result = await raftingPackageCollection.insertOne(raftingPackage);
+      res.json(result);
+    });
+    /* ------------------------------------- 
+    GET All Booking API
+    ------------------------------------- */
+    app.get('/booking', async (req, res) => {
+      const cursor = bookingCollection.find({});
+      const bookings = await cursor.toArray();
+      res.send(bookings);
+    });
+    /* ------------------------------------- 
+    GET Single Booking API
+    ------------------------------------- */
+    app.get('/booking/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const booking = await bookingCollection.findOne(query);
+      res.json(booking);
+    });
+    /* ------------------------------------- 
+    POST Single Booking API
+    ------------------------------------- */
+    app.post('/booking', async (req, res) => {
+      // Step 1. data
+      const bookingInfo = req.body;
+      // Step 2. insertOne
+      const result = await bookingCollection.insertOne(bookingInfo);
+      res.json(result);
+    });
+    /* ------------------------------------- 
+    DELETE Single Booking API
+    ------------------------------------- */
+    app.delete('/booking/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.json(result);
+    });
+    /* ------------------------------------- 
+    Update Single Booking Status API
+    ------------------------------------- */
+    /* ------------------------------------- 
+    GET All Users with Camp Booking API
+    ------------------------------------- */
+    app.get('/booking/:email', async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const cursor = bookingCollection.find(query);
+      const userBookings = await cursor.toArray();
+      res.send(userBookings);
+    });
   } finally {
     // await client.close();
   }
